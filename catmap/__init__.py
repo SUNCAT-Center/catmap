76cff9a823b46824fed94c8840a4eccb9e3f9d1c
Andrew J. Medford <ajmedfor@gmail.com>
2014-12-16 14:44:54 -0800

    Added integrated DRC solver

--- catmap/__init__.py
+++ catmap/__init__.py
@@ -24,7 +24,7 @@ from ase.structure import molecule
 from catmap.model import ReactionModel
 import data
 
-__version__ = "0.2.88"
+__version__ = "0.2.89"
 
 def load(setup_file):
     rxm = ReactionModel(setup_file = setup_file)
