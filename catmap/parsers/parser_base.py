bfca40dc95f72d8343a0750af8a5c64024840288
A.J. Medford <ajmedfor@gmail.com>
2015-06-25 22:58:32 -0700

    actually fixed tutorial 1 error

--- catmap/parsers/parser_base.py
+++ catmap/parsers/parser_base.py
@@ -34,9 +34,7 @@ class ParserBase(ReactionModelWrapper):
         #Make dictionary of useful information about species in model
         if not self.species_definitions:
             self.species_definitions = {}
-
-        if self.site_names is None:
-            self.site_names = []
+        
         for species in (self.gas_names+self.adsorbate_names+
                 self.transition_state_names+self.site_names+tuple(self._gas_sites)):
             
