1cdd0b14142836f5f35aa84c3f21bf124a9dd9b5
hoffmannm <mjhoffmann@gmail.com>
2016-02-11 15:51:36 -0800

    Made scipy dependency optional in test_dependencies.py with appropriate warning

--- test_dependencies.py
+++ test_dependencies.py
@@ -1,5 +1,8 @@
 import numpy
-import scipy
+try:
+    import scipy
+except:
+    print("Warning: scipy could not be imported! Some of the advanced features such as higher-order adsorbate-adsorbate interaction relying on arbitrary precision arithmethic may not for properly. Please install scipy for full feature support.")
 import pylab
 import mpmath
 import ase
