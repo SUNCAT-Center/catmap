1d1657e22f999fa5281c0d3a530f00df2199f252
hoffmannm <mjhoffmann@gmail.com>
2016-03-17 17:31:38 -0700

    Rearrange demo script a bit

--- tutorials/output_variables/mkm_job_output_all.py
+++ tutorials/output_variables/mkm_job_output_all.py
@@ -19,6 +19,17 @@ model.run()
 vm = analyze.VectorMap(model)
 mm = analyze.MatrixMap(model)
 
+def set_defaults(vm, mm):
+    vm.log_scale = True
+    vm.min = 1e-25
+    vm.max = 1e3
+    vm.threshold = 1e-25
+    vm.unique_only = False
+    mm.log_scale = False
+    mm.min = -2
+    mm.max = 2
+    mm.unique_only = False
+
 for output_variable in model.output_variables:
     m = mm if output_variable in two_dim_variables else vm
     plot_type = 'MatrixMap' if output_variable in two_dim_variables else 'VectorMap'
@@ -28,22 +39,12 @@ for output_variable in model.output_variables:
     # put other adjustments here
     # elif output_variable == '...':
     #     m.<attr> = '...'
-    elif output_variable == 'turnover_frequency':
-        continue  # crashes, should get fixed
     elif output_variable == 'interaction_matrix':
-        continue  # crashes, should get fixed
+        continue  # only works for model with adsorbate-adsorbate interaction
     else:  # set default values
         # not necessarily the right choice of parameters for all
         # output_variables
-        vm.log_scale = True
-        vm.min = 1e-25
-        vm.max = 1e3
-        vm.threshold = 1e-25
-        vm.unique_only = False
-        mm.log_scale = False
-        mm.min = -2
-        mm.max = 2
-        mm.unique_only = False
+        set_defaults(vm, mm)
     print(
         "Plotting {output_variable}, plot type: {plot_type}".format(**locals()))
     m.plot_variable = output_variable
