a209f46cec4022f258d359c43047b9eb2775bd11
Lynza Sprowl <halbersl@oregonstate.edu>
2017-08-02 14:41:26 -0700

    Update final version number

--- catmap/__init__.py
+++ catmap/__init__.py
@@ -41,7 +41,7 @@ from ase.structure import molecule
 from catmap.model import ReactionModel
 import data
 
-__version__ = "0.2.449"
+__version__ = "0.2.450"
 
 def griddata(*args, **kwargs):
     """Wrapper function to avoid annoying griddata errors"""
